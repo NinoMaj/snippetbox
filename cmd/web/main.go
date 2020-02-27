3f73ef5e8bb135e95da4fed24ca7af36449f841e
Nino Majder <nino.majder@gmail.com>
2020-02-27 22:41:08 +0100

    Extract routes declarations

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -24,21 +24,10 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	// Use the http.NewServeMux() function to initialize a new servemux, then
-	// register the home function as the handler for the "/" URL pattern.
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	// Serving of static files
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	// Use the http.ListenAndServe() function to start a new web server. We pass in
